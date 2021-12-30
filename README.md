9c5d8f6dcb2ecb77ad6d86dee6d64360dd7ea3d4
Brian <briancabbott@gmail.com>
2021-12-30 08:18:41 -0800

    init commit #1
    updating README for doc/name change on Custom Authorizer terminology
    
    Working on pipeline
    
    Working on pipeline
    
    init commit to my branch.
    
    updates
    
    added JWK caching support
    
    simplified logging and object packaging
    
    adding updates
    
    updating gitignore to exclude our secret
    
    adding events.json to .gitignore
    
    removed openid-config artifacts
    
    moved base64 and verification code to seperate modules
    
    updated go.mod
    
    updating gitignore
    
    basic cleanups
    
    udpated README with the HW issue in a ToDo's section
    
    cleaning up build for auto-event file generation/handling
    
    restructured package/module definitions.
    
    updates using Lestrrat Verification
    
    Removing unused code
    
    resolved compilation issues re printf formatting
    
    Adding reference example for verification
    
    working rev 1.0
    
    cleanup, finalization

--- README.md
+++ README.md
@@ -1,13 +1,50 @@
 # aws-oidc-custom-authorizer
 
-An AWS API Gateway [Custom Authorizer](http://docs.aws.amazon.com/apigateway/latest/developerguide/use-custom-authorizer.html) that authorizes API requests against an OIDC provider.
+An AWS API Gateway [Lambda Authorizer](https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-use-lambda-authorizer.html) that authorizes API requests against an OIDC provider.
 This authorizer is written in golang and compiled to arm64 to maximize performance and minimize execution costs.
+
+## Requirements
+    - This will accept a AUTHORITY and AUDIENCE environment variable and be able to take OUATH2 JWT tokens and verify them.
+
+## Libraries
+    - https://github.com/lestrrat-go/jwx
+    - https://github.com/hkra/go-jwks
 
 ## Maintainers
 
 - [erikrj](https://github.com/erikrj)
+- [briancabbott](https://github.com/briancabbott)
 
 ## License
 
 The contents of this repository are under the BSD 3-Clause license. See the
 license [here](https://github.com/truemark/aws-cli-docker/blob/main/LICENSE.txt).
+
+
+## Dev Notes (temp section)
+
+### x86_64 / ARM64 targets for dev vs deploy
+We need to setup the build to produce targets depending on dev vs deploy - the local builds need 
+the make target setup to build a x86_64 target for a local docker container deployment. See:
+   - https://docs.aws.amazon.com/lambda/latest/dg/golang-package.html
+
+   
+## TODOs
+
+### Multi-targeted builds
+We still need to figure out how to nicely sense the HW were compiling on and pic 
+the appropriate metal for it... 
+
+### Use Letstrat's verification API instead of manaul verification
+Use the jws.VerifySet() from "github.com/lestrrat-go/jwx/jws" instead of manually 
+iterating through each key. This keeps us congruent with the standard/free updates 
+from Lestrat, etc. (just better practice)
+
+### Remove/update printfs
+Either take out the extraneous prints or move them to use Error-Logging if they are useful.
+
+### Single Package/Module
+Move everything into the authorizer package and out of individual sub-modules.
+
+### Use Viper instead of manual for envs
+Perform "out of the box" type setup activities with Viper instead of manual env-varing.
---
+++ authorizer/config.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	// Path as post-fix for OpenID Configuration URL
+	OpenidConfigUrlPostFix = ".well-known/openid-configuration"
+)
+
+type OpenIDConfig struct {
+	Issuer                             string   `json:"issuer"`
+	JWKS_URI                           string   `json:"jwks_uri"`
+	AuthorizationEndpoint              string   `json:"authorization_endpoint"`
+	TokenEndpoint                      string   `json:"token_endpoint"`
+	UserInfoEndpoint                   string   `json:"userinfo_endpoint"`
+	EndSessionEndpoint                 string   `json:"end_session_endpoint"`
+	CheckSessionIFrame                 string   `json:"check_session_iframe"`
+	RevocationEndpoint                 string   `json:"revocation_endpoint"`
+	IntrospectionEndpoint              string   `json:"introspection_endpoint"`
+	DeviceAuthorizationEndpoint        string   `json:"device_authorization_endpoint"`
+	FrontChannelLogoutSupported        bool     `json:"frontchannel_logout_supported"`
+	FrontChannelLogoutSessionSupported bool     `json:"frontchannel_logout_session_supported"`
+	BackChannelLogoutSupported         bool     `json:"backchannel_logout_supported"`
+	BackChannelLogoutSessionSupported  bool     `json:"backchannel_logout_session_supported"`
+	ScopesSupported                    []string `json:"scopes_supported"`
+	ClaimsSupported                    []string `json:"claims_supported"`
+	GrantTypesSupported                []string `json:"grant_types_supported"`
+	ResponseTypesSupported             []string `json:"response_types_supported"`
+	ResponseModesSupported             []string `json:"response_modes_supported"`
+	TokenEndpointAuthMethodsSupported  []string `json:"token_endpoint_auth_methods_supported"`
+	IdTokenSigningAlgValuesSupported   []string `json:"id_token_signing_alg_values_supported"`
+	SubjectTypesSupported              []string `json:"subject_types_supported"`
+	CodeChallengeMethodsSupported      []string `json:"code_challenge_methods_supported"`
+	RequestParameterSupported          bool     `json:"request_parameter_supported"`
+}
+
+type Config struct {
+	AuthorityEnv    string
+	AudienceEnv     string
+	OpenIDConfigURL string // TODO: not sure we need this...
+	OpenIDConfig    *OpenIDConfig
+}
+
+func SetupConfig() (*Config, error) {
+	authorityEnv := os.Getenv("AUTHORITY")
+	audienceEnv := os.Getenv("AUDIENCE")
+
+	openIdConfigURL := authorityEnv + OpenidConfigUrlPostFix
+	if strings.HasPrefix(openIdConfigURL, "http://") {
+		return nil, errors.New("HTTP URL values for the AUTHORITY environment-variable is unsupported.")
+	}
+	openIdConfigURL = strings.Replace(openIdConfigURL, "https://", "", 1)
+	if strings.Contains(openIdConfigURL, "//") {
+		openIdConfigURL = strings.Replace(openIdConfigURL, "//", "/", -1)
+	}
+	openIdConfigURL = "https://" + openIdConfigURL
+	log.Debug().
+		Str("openIdConfigURL", openIdConfigURL).
+		Msg("OpenID Configuration Data URL")
+	openIdConfig := getOpenIDConfiguration(openIdConfigURL)
+
+	config := Config{
+		AuthorityEnv:    authorityEnv,
+		AudienceEnv:     audienceEnv,
+		OpenIDConfigURL: openIdConfigURL,
+		OpenIDConfig:    openIdConfig,
+	}
+	return &config, nil
+}
+
+func getOpenIDConfiguration(url string) *OpenIDConfig {
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Debug().Msg("HTTP GET Request Completed")
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var openIDConfig OpenIDConfig
+	json.Unmarshal(body, &openIDConfig)
+	log.Debug().
+		Str("body", string(body)).
+		Msg("OpenID (body) Configuration Struct successfully unmarshalled")
+
+	return &openIDConfig
+}
