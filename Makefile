da073b279b98c7aa5c86f8d411749eb2f1197d5c
Brian <briancabbott@gmail.com>
2022-02-22 14:31:29 -0800

    rebased initial revision.

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
-.PHONY: build
+.PHONY: setupevt clean build run
+
+TOK := $(shell ./get_token.sh);
+EVENT_FILE := "./event.json"
+
+setupevent:
+	./get_token.sh > $(EVENT_FILE)
+
+clean:
+	rm -rf .aws-sam
+	rm -f $(EVENT_FILE)
 
 build:
-	sam build
+	sam build --debug
+
+run:
+	sam local invoke "AuthorizerFunction" -e $(EVENT_FILE)
--- authorizer/main.go
+++ authorizer/main.go
@@ -1,51 +1,112 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
+	"github.com/rs/zerolog/log"
 )
 
 var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	ErrStatusCode     = 500
+	SuccessStatusCode = 200
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+func getToken(requestHeader string) (string, error) {
+	match, _ := regexp.Match(`Bearer (.*)`, []byte(requestHeader))
+	log.Debug().
+		Bool("match", match).
+		Str("authorizationToken", requestHeader).
+		Msg("Header Matched for AuthorizationToken on Bearer")
+
+	if !match {
+		errMsg := fmt.Sprintf("Invalid Authorization token - %s does not match \"Bearer .*\"\n", requestHeader)
+		return "", errors.New(errMsg)
+	}
+	r, _ := regexp.Compile(`Bearer (.*)`)
+	matchedToken := r.FindString(requestHeader)
+	keyToken := strings.Replace(matchedToken, "Bearer ", "", 1)
+	log.Debug().
+		Str("matchedToken", keyToken).
+		Msg("DELETE-ME: matchedToken Found is:")
+
+	return keyToken, nil
+}
+
+func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayProxyResponse, error) {
+	LogRequest(event)
+
+	// Setup our Config object
+	config, err := SetupConfig()
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		LogError(err)
+		return events.APIGatewayProxyResponse{
+			Body:       "ERROR on Setting up Configuration data-structure for aws-oidc-authorizer-lambda",
+			StatusCode: ErrStatusCode,
+		}, err
 	}
+	LogConfig(config)
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
+	// Setup JWK and retreive our cached key...
+	ar := jwk.NewAutoRefresh(ctx)
+	ar.Configure(config.OpenIDConfig.JWKS_URI, jwk.WithMinRefreshInterval(15*time.Minute))
+	keySet, err := ar.Refresh(ctx, config.OpenIDConfig.JWKS_URI)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+		LogError(err)
+		return events.APIGatewayProxyResponse{
+			// TODO: Update/Enhance messaging for err handling - do we need a JSON struct here in the body? etc...
 
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
+			Body:       "ERROR on Setting up JWK AUTO-REFRESH TODO::MAKE ME BETTER",
+			StatusCode: ErrStatusCode,
+		}, err
 	}
+	LogKeySet(keySet)
+
+	bearer := event.AuthorizationToken
+	authToken, err := getToken(bearer)
+	if err != nil {
+		LogError(err)
+		return events.APIGatewayProxyResponse{
+			// TODO: Update/Enhance messaging for err handling - do we need a JSON struct here in the body? etc...
+			Body:       "ERROR on GetToken TODO::MAKE ME BETTER",
+			StatusCode: ErrStatusCode,
+		}, err
+	}
+	// Perform main verification
+	token, err := jwt.Parse(
+		[]byte(authToken),
+		jwt.WithValidate(true),
+		jwt.WithKeySet(keySet),
+		jwt.WithAudience(config.AudienceEnv),
+	)
+	if err != nil {
+		LogError(err)
+		return events.APIGatewayProxyResponse{
+			// TODO: What do we want to supply in the body for error handling, etc...
+			Body:       "ERROR on VerifyToken TODO::MAKE ME BETTER",
+			StatusCode: ErrStatusCode,
+		}, err
+	}
+	LogToken(token)
+	_ = token
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
-		StatusCode: 200,
+		Body:       "Verification Success",
+		StatusCode: SuccessStatusCode,
 	}, nil
 }
 
+// TODO: Since this is a secure/sensitive app, we need to determine what can and cant be logged into AWS, etc
+
 func main() {
+	Init()
 	lambda.Start(handler)
 }
